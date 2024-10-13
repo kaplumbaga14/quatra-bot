74077c871a16e9f293e19920642dd92601812ddd
Surungen <19358576-Surungen@users.noreply.replit.com>
2024-10-13 13:46:26 +0000

    a

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const { Client, Collection, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-const config = require('./config.json');
-const { getSetting } = require('./database');
+const {
+    Client,
+    Collection,
+    GatewayIntentBits,
+    EmbedBuilder,
+} = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+const config = require("./config.json");
+const { getSetting } = require("./database");
+const express = require("express");
+const app = express();
+const port = 3100;
+
+app.get("/", (req, res) => res.send("we discord"));
+
+app.listen(port, () =>
+    console.log(`Bot bu adres üzerinde çalışıyor: http://localhost:${port}`),
+);
 
 const client = new Client({
     intents: [
@@ -17,24 +31,30 @@ const client = new Client({
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(commandsPath);
 
 for (const folder of commandFolders) {
     const folderPath = path.join(commandsPath, folder);
-    const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+    const commandFiles = fs
+        .readdirSync(folderPath)
+        .filter((file) => file.endsWith(".js"));
     for (const file of commandFiles) {
         const filePath = path.join(folderPath, file);
         const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
+        if ("data" in command && "execute" in command) {
             client.commands.set(command.data.name, command);
         }
     }
 }
 
-const { handleStartApplication, handleApplicationSubmit, handleApplicationResponse } = require('./commands/basvuru/basvuru');
+const {
+    handleStartApplication,
+    handleApplicationSubmit,
+    handleApplicationResponse,
+} = require("./commands/basvuru/basvuru");
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
     if (interaction.isCommand()) {
         const command = client.commands.get(interaction.commandName);
         if (!command) return;
@@ -43,95 +63,119 @@ client.on('interactionCreate', async interaction => {
             await command.execute(interaction);
         } catch (error) {
             console.error(error);
-            await interaction.reply({ content: 'Komut yürütülürken bir hata oluştu!', ephemeral: true });
+            await interaction.reply({
+                content: "Komut yürütülürken bir hata oluştu!",
+                ephemeral: true,
+            });
         }
     } else if (interaction.isButton()) {
-        if (interaction.customId === 'start_application') {
+        if (interaction.customId === "start_application") {
             await handleStartApplication(interaction);
-        } else if (interaction.customId.startsWith('kabul_') || interaction.customId.startsWith('red_')) {
+        } else if (
+            interaction.customId.startsWith("kabul_") ||
+            interaction.customId.startsWith("red_")
+        ) {
             await handleApplicationResponse(interaction);
         }
-    } else if (interaction.isModalSubmit() && interaction.customId === 'application_modal') {
+    } else if (
+        interaction.isModalSubmit() &&
+        interaction.customId === "application_modal"
+    ) {
         await handleApplicationSubmit(interaction);
     }
 });
 
-client.once('ready', () => {
+client.once("ready", () => {
     console.log(`${client.user.tag} olarak giriş yapıldı!`);
     loadSlashCommands();
 });
 
 async function loadSlashCommands() {
-    const commandsPath = path.join(__dirname, 'commands');
+    const commandsPath = path.join(__dirname, "commands");
     const commandFolders = fs.readdirSync(commandsPath);
 
     for (const folder of commandFolders) {
         const folderPath = path.join(commandsPath, folder);
-        const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+        const commandFiles = fs
+            .readdirSync(folderPath)
+            .filter((file) => file.endsWith(".js"));
         for (const file of commandFiles) {
             const filePath = path.join(folderPath, file);
             const command = require(filePath);
-            if ('data' in command && 'execute' in command) {
+            if ("data" in command && "execute" in command) {
                 await client.application.commands.create(command.data);
                 console.log(`Yüklenen komut: ${command.data.name}`);
             } else {
-                console.log(`[UYARI] ${filePath} komutunda gerekli "data" veya "execute" özelliği eksik.`);
+                console.log(
+                    `[UYARI] ${filePath} komutunda gerekli "data" veya "execute" özelliği eksik.`,
+                );
             }
         }
     }
 }
 
-client.on('messageUpdate', async (oldMessage, newMessage) => {
+client.on("messageUpdate", async (oldMessage, newMessage) => {
     if (oldMessage.author.bot) return;
-    const logChannel = await getLogChannel(oldMessage.guild, 'genelLogKanal');
+    const logChannel = await getLogChannel(oldMessage.guild, "genelLogKanal");
     if (logChannel) {
         const embed = new EmbedBuilder()
-            .setTitle('Mesaj Düzenlendi')
-            .setColor('#00ff00')
-            .setDescription(`**Kanal:** ${oldMessage.channel}\n**Yazar:** ${oldMessage.author.tag}\n**Eski Mesaj:** ${oldMessage.content}\n**Yeni Mesaj:** ${newMessage.content}`)
+            .setTitle("Mesaj Düzenlendi")
+            .setColor("#00ff00")
+            .setDescription(
+                `**Kanal:** ${oldMessage.channel}\n**Yazar:** ${oldMessage.author.tag}\n**Eski Mesaj:** ${oldMessage.content}\n**Yeni Mesaj:** ${newMessage.content}`,
+            )
             .setTimestamp();
         await logChannel.send({ embeds: [embed] });
     }
 });
 
-client.on('guildBanAdd', async (ban) => {
-    const logChannel = await getLogChannel(ban.guild, 'moderasyonLogKanal');
+client.on("guildBanAdd", async (ban) => {
+    const logChannel = await getLogChannel(ban.guild, "moderasyonLogKanal");
     if (logChannel) {
         const embed = new EmbedBuilder()
-            .setTitle('Kullanıcı Banlandı')
-            .setColor('#ff0000')
-            .setDescription(`**Kullanıcı:** ${ban.user.tag}\n**Sebep:** ${ban.reason || 'Sebep belirtilmedi'}`)
+            .setTitle("Kullanıcı Banlandı")
+            .setColor("#ff0000")
+            .setDescription(
+                `**Kullanıcı:** ${ban.user.tag}\n**Sebep:** ${ban.reason || "Sebep belirtilmedi"}`,
+            )
             .setTimestamp();
         await logChannel.send({ embeds: [embed] });
     }
 });
 
-client.on('guildBanRemove', async (ban) => {
-    const logChannel = await getLogChannel(ban.guild, 'moderasyonLogKanal');
+client.on("guildBanRemove", async (ban) => {
+    const logChannel = await getLogChannel(ban.guild, "moderasyonLogKanal");
     if (logChannel) {
         const embed = new EmbedBuilder()
-            .setTitle('Kullanıcı Banı Kaldırıldı')
-            .setColor('#00ff00')
-            .setDescription(`**Kullanıcı:** ${ban.user.tag}\n**Sebep:** ${ban.reason || 'Sebep belirtilmedi'}`)
+            .setTitle("Kullanıcı Banı Kaldırıldı")
+            .setColor("#00ff00")
+            .setDescription(
+                `**Kullanıcı:** ${ban.user.tag}\n**Sebep:** ${ban.reason || "Sebep belirtilmedi"}`,
+            )
             .setTimestamp();
         await logChannel.send({ embeds: [embed] });
     }
 });
-client.on('guildMemberRemove', async (member) => {
+client.on("guildMemberRemove", async (member) => {
     const fetchedLogs = await member.guild.fetchAuditLogs({
         limit: 1,
-        type: 'MEMBER_KICK',
+        type: "MEMBER_KICK",
     });
     const kickLog = fetchedLogs.entries.first();
 
     if (kickLog && kickLog.target.id === member.id) {
-        const logChannel = await getLogChannel(member.guild, 'moderasyonLogKanal');
+        const logChannel = await getLogChannel(
+            member.guild,
+            "moderasyonLogKanal",
+        );
         if (logChannel) {
             const { executor, reason } = kickLog;
             const embed = new EmbedBuilder()
-                .setTitle('Kullanıcı Kicklendi')
-                .setColor('#ff9900')
-                .setDescription(`**Kullanıcı:** ${member.user.tag}\n**Kickleyen:** ${executor.tag}\n**Sebep:** ${reason || 'Sebep belirtilmedi'}`)
+                .setTitle("Kullanıcı Kicklendi")
+                .setColor("#ff9900")
+                .setDescription(
+                    `**Kullanıcı:** ${member.user.tag}\n**Kickleyen:** ${executor.tag}\n**Sebep:** ${reason || "Sebep belirtilmedi"}`,
+                )
                 .setTimestamp();
             await logChannel.send({ embeds: [embed] });
         }
@@ -145,7 +189,10 @@ async function getLogChannel(guild, settingKey) {
 
         if (channelId) {
             const channel = await guild.channels.fetch(channelId);
-            console.log(`Bulunan kanal:`, channel ? channel.name : 'Kanal bulunamadı');
+            console.log(
+                `Bulunan kanal:`,
+                channel ? channel.name : "Kanal bulunamadı",
+            );
             return channel;
         } else {
             console.log(`${settingKey} için kanal ID'si bulunamadı`);
@@ -156,29 +203,33 @@ async function getLogChannel(guild, settingKey) {
     return null;
 }
 
-client.on('messageDelete', async message => {
+client.on("messageDelete", async (message) => {
     if (message.author.bot) return;
-    
+
     try {
-        const logChannel = await getLogChannel(message.guild, 'genelLogKanal');
-        console.log('Silinen mesaj için log kanalı:', logChannel ? logChannel.name : 'Bulunamadı');
+        const logChannel = await getLogChannel(message.guild, "genelLogKanal");
+        console.log(
+            "Silinen mesaj için log kanalı:",
+            logChannel ? logChannel.name : "Bulunamadı",
+        );
 
         if (logChannel) {
             const embed = new EmbedBuilder()
-                .setTitle('Mesaj Silindi')
-                .setColor('#ff0000')
-                .setDescription(`**Kanal:** ${message.channel}\n**Yazar:** ${message.author.tag}\n**Mesaj:** ${message.content}`)
+                .setTitle("Mesaj Silindi")
+                .setColor("#ff0000")
+                .setDescription(
+                    `**Kanal:** ${message.channel}\n**Yazar:** ${message.author.tag}\n**Mesaj:** ${message.content}`,
+                )
                 .setTimestamp();
 
             await logChannel.send({ embeds: [embed] });
-            console.log('Log mesajı gönderildi');
+            console.log("Log mesajı gönderildi");
         } else {
-            console.log('Log kanalı bulunamadı, mesaj gönderilemedi');
+            console.log("Log kanalı bulunamadı, mesaj gönderilemedi");
         }
     } catch (error) {
-        console.error('Mesaj silme logu gönderilirken hata oluştu:', error);
+        console.error("Mesaj silme logu gönderilirken hata oluştu:", error);
     }
 });
-
 
 client.login(config.token);
